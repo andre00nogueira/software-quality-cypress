dcb0a8b0408f9917fafe2488e1b76b180bc4383d
André Silva <andre.silva@voidsoftware.com>
2022-03-29 11:58:30 +0100

    Feature: implement more tests

--- cypress/integration/ManageInvoiceItems/addInvoice.spec.js
+++ cypress/integration/ManageInvoiceItems/addInvoice.spec.js
@@ -13,3 +13,7 @@ When('I click on the button with label {string}', (actionName) => {
 Then('a new empty item is created', () => {
     cy.get('[id^=item_]').should('have.length', 2);
 })
+
+And('the total price should be {string}', (total) => {
+    cy.get('#totalPrice').contains(total)
+});
