22c4120ca317057206a5f6199d99df8c3e11e526
Alexandre Santos <santosalexandre389@gmail.com>
2022-05-19 19:47:19 +0100

    Preparing cypress to run on pipeline

--- cypress/integration/GenerateInvoices/generateInvoices.js
+++ cypress/integration/GenerateInvoices/generateInvoices.js
@@ -1,6 +1,6 @@
 import {Given} from "cypress-cucumber-preprocessor/steps";
 
-const url = 'http://localhost:3000/'
+const url = Cypress.env('APP_URL');
 Given('I open the main page', () => {
     cy.visit(url)
 })
