35adb4d5ca0289c837dbbe388d6bf2762d9e682d
André Silva <andre.silva@voidsoftware.com>
2022-06-15 15:45:07 +0100

    Add tests for checking same as shipping checkbox

--- cypress/integration/ManageShippingAddress.feature
+++ cypress/integration/ManageShippingAddress.feature
@@ -7,3 +7,11 @@ Feature: Manage Shipping Address
     When The page is loaded
     Then I see the "Same as Shipping" button is not checked
     And all the fields of the billing address are not readonly
+
+  Scenario: Mark Billing Address same as Shipping
+    When I write "Rua Teste" in the field "shipToAddress"
+    And I click on the button "Same as Shipping"
+    Then I see the field "billToAddress" is equal to "Rua Teste"
+    And I see the field "shipToAddress" is equal to "Rua Teste"
+    And I see the field "billToAddress" is readonly
+    And I see the "Same as Shipping" button is checked
--- cypress/integration/ManageShippingAddress/manageShippingAddress.js
+++ cypress/integration/ManageShippingAddress/manageShippingAddress.js
@@ -27,3 +27,30 @@ And('all the fields of the billing address are not readonly', () => {
     const billingFromAddress = cy.get('input[name="billToAddress"]')
     billingFromAddress.should('not.have.attr', 'readonly', 'readonly')
 })
+
+When('I write {string} in the field {string}', (valueToWrite, field) => {
+    const shipToAddress = cy.get(`input[name="${field}"]`)
+
+    shipToAddress.type(valueToWrite)
+})
+
+And('I click on the button {string}', (button) => {
+    const input = getInputByLabel(button)
+    input.click()
+})
+
+Then('I see the field {string} is equal to {string}', (firstField, value) => {
+    const field = cy.get(`input[name="${firstField}"]`)
+
+    field.should('have.value', value)
+})
+
+And('I see the field {string} is readonly', (field) => {
+    const billToAddress = cy.get(`input[name="${field}"]`)
+    billToAddress.should('have.attr', 'readonly', 'readonly')
+})
+
+And('I see the {string} button is checked', (button) => {
+    const input = getInputByLabel(button)
+    input.should('be.checked')
+})
